6592a2d18d591b7077cf444d46c7f3149a94b166
Alex Cegarra <Alex@Alexs-MacBook-Pro-4.local>
2017-11-30 01:56:29 -0800

    downloaded and added a repo for eval of embeddings, added a tokenizer, implemented naive optimization of embedding

--- vector_flow.py
+++ vector_flow.py
@@ -1,21 +1,31 @@
 import gensim
 from gensim.models import Word2Vec as Word2Vec
-import sys, re
+import sys, re, os
 import nltk
 from nltk.tokenize import word_tokenize
-SENTENCE_TOKENIZER = nltk.data.load('tokenizers/punkt/english.pickle') 
+from eval_embedding import eval_sim
+SENTENCE_TOKENIZER = nltk.data.load('./nltk_data/tokenizers/punkt/english.pickle') 
 QUOTES = re.compile("\u201c|\u201d")
 
+def read_dir(path):
+	"""
+	@param path: (type = str) path to dir that containes files
+	@return: (type=str)
+	"""
+	assert os.path.exists(path)
+	file_names = os.listdir(path)
+	return read(file_names, path)
 
-def read(file_names):
+def read(file_names, path):
 	"""
 	Reads in a text file as a string, returning the stringified version
 	@param file_names: (type=list<str>) files to be read
+	@param path: (type = str) path to dir that containes files
 	@return: (type=str)
 	"""
 	data = ""
 	for file_name in file_names:
-		with open(file_name, "r") as f:
+		with open(path + "/" + file_name, "r") as f:
 			data += re.sub(QUOTES, "\"", f.read())
 			data += "\n"
 	return data
@@ -30,7 +40,7 @@ def parse(text):
 	sentences = [word_tokenize(sentence) for sentence in sentences]
 	return sentences
 
-def init_model(sentences, min_count=2, window=8, dim=40):
+def init_model(sentences):
 	"""
 	Initializes a Word2Vec model
 	@param sentences: (type=list<list<str>>) a list of lists of words
@@ -39,10 +49,30 @@ def init_model(sentences, min_count=2, window=8, dim=40):
 	@param dim: (type=int) dimension of word vectors
 	@return: (type=Word2Vec) model
 	"""
-	model = Word2Vec(sentences, min_count=min_count, window=window, size=dim)
-	return model
+	best_spearmen = 0
+	best_model = None
+	best_params = (0,0,0)
+	count = 0
+	for dim in range(20, 61):
+		for window in range(5, 11):
+			for min_count in range(2,5):
+				model = Word2Vec(sentences, min_count=min_count, window=window, size=dim)
+				similarity_spearmen = eval_sim(model)
+				if similarity_spearmen > best_spearmen:
+					best_model = model
+					best_params = (min_count, window, dim)
+					best_spearmen = similarity_spearmen
+				print(str(count) + ": " + str(similarity_spearmen))
+				count += 1
+	print("min_count: " + str(best_params[0]), 
+		"window: " + str(best_params[1]), 
+		"dim: " + str(best_params[2]))
+	print(best_spearmen)
+	return best_model
 
-data = read(sys.argv[2:])
+#TO EXECUTE ON COMMAND LINE
+#python vector_flow.py <model_file_name> <path_to_dir>
+data = read_dir(sys.argv[2])
 sentences = parse(data)
 model = init_model(sentences)
 model.save(sys.argv[1])
